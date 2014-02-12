35bfad1081fc1a5e2a506cc43c3f2d699b7dd05e
Dave Hull <dpgit@trustedsignal.com>
2014-02-11 23:02:33 -0800

    Update Get-HostData.ps1
    Multiple bug fixes, mostly due to typos.

--- Get-HostData.ps1
+++ Get-HostData.ps1
@@ -79,7 +79,7 @@ $handleout = $temp + "\" + $this_computer + "_handle.txt"
 
 # get image file execution options
 $imgxoptout = $temp + "\" + $this_computer + "_imgexecopt.txt"
-& reg query "HKLM\software\microsoft\windows nt\currentversion\image file execution options" /s | set-content -encoding ascii $imgexecopt
+& reg query "HKLM\software\microsoft\windows nt\currentversion\image file execution options" /s | set-content -encoding ascii $imgxoptout
   
 
 # get bits transfers
@@ -93,7 +93,7 @@ $($(foreach ($svc in (& c:\windows\system32\sc query)) {
   if ($svc -match "SERVICE_NAME:\s(.*)") {
     & c:\windows\system32\sc qtriggerinfo $($matches[1])
   }
-})|?{$_.length -gt 1 -and $_ -notmatch "\[SC\] QueryServiceConfig2 SUCCESS|has not registered for any" }) | set-content -encoding Ascii $svctriggers
+})|?{$_.length -gt 1 -and $_ -notmatch "\[SC\] QueryServiceConfig2 SUCCESS|has not registered for any" }) | set-content -encoding Ascii $svctrigout
 
 
 # check for locked files
