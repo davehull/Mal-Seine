a386de4fcbfd224f9aa97d7a2b54c4b387f2a0fe
Dave Hull <dpgit@trustedsignal.com>
2014-02-18 11:31:13 -0800

    Update Convert-BitsToSV.ps1
    Just capturing some things for future reference.

--- Convert-BitsToSV.ps1
+++ Convert-BitsToSV.ps1
@@ -1,4 +1,7 @@
 <#
 .SYNOPSIS
 A placeholder for a script to turn BITS transfer data to separated values format suitable for analysis.
+.NOTES
+$data = Import-Clixml .\LUHAD_bitsxfer.xml
+$($data | % { $_; $_.FileList|% {$_}}) | fl *
 #>
