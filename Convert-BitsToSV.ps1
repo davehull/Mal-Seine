413388d05dd84233a05430258484e3311529fe6e
Dave Hull <dpgit@trustedsignal.com>
2014-02-18 21:17:09 -0800

    Update Convert-BitsToSV.ps1
    Works.

--- Convert-BitsToSV.ps1
+++ Convert-BitsToSV.ps1
@@ -1,7 +1,85 @@
 <#
 .SYNOPSIS
-A placeholder for a script to turn BITS transfer data to separated values format suitable for analysis.
-.NOTES
-$data = Import-Clixml .\LUHAD_bitsxfer.xml
-$($data | % { $_; $_.FileList|% {$_}}) | fl *
+Convert-BitsXfersToSV.ps1 takes the output from Get-HostData.ps1's Bits Transfer 
+collection and parses the FileList data into delimited format suitable for stack 
+ranking via get-stakrank.ps1.
+
+.PARAMETER FileNamePattern
+Specifies the naming pattern common to the handle file output to be converted.
+.PARAMETER Delimiter
+Specifies the delimiter character to use for output. Tab is default.
+.PARAMETER ToFile
+Specifies that output be written to a file matching the FileNamePattern (same path),
+but with .tsv or .csv extension depending on delimtier (.tsv is default).
 #>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+        [string]$FileNamePattern,
+    [Parameter(Mandatory=$False,Position=1)]
+        [string]$Delimiter="`t",
+    [Parameter(Mandatory=$False,Position=2)]
+        [switch]$tofile=$False
+)
+
+function Get-Files {
+<#
+.SYNOPSIS
+Returns the list of input files matching the user supplied file name pattern.
+Traverses subdirectories.
+#>
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+        [String]$FileNamePattern
+)
+    Write-Verbose "Entering $($MyInvocation.MyCommand)"
+    Write-Verbose "Looking for files matching user supplied pattern, $FileNamePattern"
+    Write-Verbose "This process traverses subdirectories so it may take some time."
+    $Files = @(ls -r $FileNamePattern | % { $_.FullName })
+    if ($Files) {
+        Write-Verbose "File(s) matching pattern, ${FileNamePattern}:`n$($Files -join "`n")"
+        $Files
+    } else {
+        Write-Error "No input files were found matching the user supplied pattern, `
+            ${FileNamePattern}."
+        Write-Verbose "Exiting $($MyInvocation.MyCommand)"
+        exit
+    }
+    Write-Verbose "Exiting $($MyInvocation.MyCommand)"
+}
+
+function Convert {
+Param(
+    [Parameter(Mandatory=$True,Position=0)]
+        [String]$File,
+    [Parameter(Mandatory=$True,Position=1)]
+        [char]$Delimiter
+)
+    Write-Verbose "Entering $($MyInvocation.MyCommand)"
+    Write-Verbose "Processing $File."
+    $RemoteName = $Localname = $IsTransferComplete = $BytesTotal = $BytesTransferrred = $False
+    $data = Import-Clixml $File
+    $data | % { $_.FileList } | ConvertTo-Csv -NoTypeInformation -Delimiter $Delimiter
+}
+
+$Files = Get-Files -FileNamePattern $FileNamePattern
+
+foreach ($File in $Files) {
+    $data = Convert $File $Delimiter
+    if ($tofile) {
+        $path = ls $File
+        $outpath = $path.DirectoryName + "\" + $path.BaseName
+        if ($Delimiter -eq "`t") {
+            $outpath += ".tsv"
+        } elseif ($Delimiter -eq ",") {
+            $outpath += ".csv"
+        } else {
+            $outpath += ".sv"
+        }
+        Write-Verbose "Writing output to ${outpath}."
+        $data | Select -Unique | Set-Content -Encoding Ascii $outpath
+    } else {
+        $data | Select -Unique
+    }
+}
